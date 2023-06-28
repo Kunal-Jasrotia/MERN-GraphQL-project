574c71b4b7fff0d35c8277ba45323f1f78a86563
kunal-6421 <123140152+kunal-6421@users.noreply.github.com>
2023-06-28 20:38:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-import express from 'express';
-import { graphqlHTTP } from 'express-graphql'
-import { buildSchema } from 'graphql'
+const express = require('express')
+const { graphqlHTTP } = require('express-graphql')
+const { buildSchema, execute } = require('graphql')
 const app = express();
 
 app.use(express.json())
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
     type RootQuery{
-         
+         events: [String!]!
     }
 
     type RootMutation{
-
+        createEvent(name:String):String
     }
 
     schema {
@@ -19,7 +19,16 @@ app.use('/graphql', graphqlHTTP({
         mutation:RootMutation
     }
     `),
-    rootValue: {}
+    rootValue: {
+        events: () => {
+            return ['a', 'b', 'c']
+        },
+        createEvent: (args) => {
+            let eventName = args.name
+            return eventName
+        }
+    },
+    graphiql: true
 }))
 
 app.listen(3000);
