711564942d19de8a691aa2c137d01f7577ad895b
kunal-6421 <123140152+kunal-6421@users.noreply.github.com>
2023-07-16 14:24:21 +0530

    completed

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-const express = require('express')
-const { graphqlHTTP } = require('express-graphql')
-const { buildSchema, execute } = require('graphql')
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const mongoose = require('mongoose');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+
 const app = express();
 
-app.use(express.json())
-app.use('/graphql', graphqlHTTP({
-    schema: buildSchema(`
-    type RootQuery{
-         events: [String!]!
-    }
+app.use(bodyParser.json());
 
-    type RootMutation{
-        createEvent(name:String):String
-    }
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
-    schema {
-        query:RootQuery
-        mutation:RootMutation
-    }
-    `),
-    rootValue: {
-        events: () => {
-            return ['a', 'b', 'c']
-        },
-        createEvent: (args) => {
-            let eventName = args.name
-            return eventName
-        }
-    },
+app.use(isAuth);
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true
-}))
+  })
+);
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-ntrwp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+  )
+  .then(() => {
+    app.listen(8000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
